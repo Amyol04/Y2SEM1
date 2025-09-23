459e8d9e7320d1f0508f5a714924b97ec5c91a35
Amy O Leary! <amyoleary404@gmail.com>
2025-09-23 13:32:29 +0100

    Update Factorial.java

--- Y2SEM1/LinerarData/COMP7035/LabTwo/Factorial.java
+++ Y2SEM1/LinerarData/COMP7035/LabTwo/Factorial.java
@@ -3,16 +3,18 @@ package LabTwo;
 public class Factorial {
     private static int factorial(int n) {
 
-        int result = 1;
-        // Task2
+        int result = 1; //Initialize result to 1 (since factorial starts from 1)
+
+        // Task2 Loop from 2 to n and multiply each number with result
         for (int factor =2; factor <= n; factor++){
-            result *= factor;
+
+            result *= factor; // multiply result bu current factor. 
 
         }
         return result;
     }
 
     public static void main(String[] args) {
-        System.out.println(factorial(5));
+        System.out.println(factorial(5));// puts factoral number into code. 
     }
 }
