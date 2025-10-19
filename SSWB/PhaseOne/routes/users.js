9da1b7c48b91e6cb89d8ae546703f0a04569b160
Amy O Leary! <amyoleary404@gmail.com>
2025-10-19 16:52:42 +0100

    Delete SSWB/PhaseOne/routes/users.js

--- SSWB/PhaseOne/routes/users.js
+++
@@ -1,9 +0,0 @@
-var express = require('express'); // imports express
-var router = express.Router(); // creats router object 
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
-module.exports = router;
