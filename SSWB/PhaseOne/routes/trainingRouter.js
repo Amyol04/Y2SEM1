efd6c620b705d8dbb5afe823f1d1a09a841234ba
Amy O Leary! <amyoleary404@gmail.com>
2025-10-19 16:52:02 +0100

    Update trainingRouter.js

--- SSWB/PhaseOne/routes/trainingRouter.js
+++ SSWB/PhaseOne/routes/trainingRouter.js
@@ -3,15 +3,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const Training = require('../models/training'); // Your Mongoose model
+
+const Training = require('../models/training');
+
+
 const trainingRouter = express.Router();
 
-trainingRouter.use(bodyParser.json());
-
 trainingRouter.route('/')
-.get((req, res, next) => {
-    res.end("Hello");
+.get((req,res,next) => {
+    res.end("Hello")
 })
+
 
 // creating the booking
 trainingRouter.route('/create')
@@ -19,18 +21,13 @@ trainingRouter.route('/create')
     res.render('newtraining.ejs', {title: 'New Training'})
 })
 
-trainingRouter.route('/traininglist')
-.get((req,red,next) =>{
-    res.render('traininglist.ejs', {title: 'training List'})
-})
-
 
 .post((req, res, next) => {
     Training.create(req.body) // the model.create creates a single or multiple documents
     .then((trainingcreated) => { // the request body contains the info of the training
         Training.find() // to check if there are any trainings
-        .then((trainingsfound) => { // if there are trainings, they are stored in trainings
-            res.render('traininglist', { 'traininglist': trainingsfound, title: 'All trainings' });
+        .then((trainingsfound) => { // if there are trainings, they are stored in trainingsfound
+            res.render('traininglist', { 'traininglist': trainingsfound, title: 'All Trainings' }); // displays all trainings. 
         }, (err) => next(err))
         .catch((err) => next(err));
     }, (err) => next(err))
